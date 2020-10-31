2fe594656acef102374b117ce1b05bce72fc7630
srcortez45 <jcortezmg@gmail.com>
2020-10-30 21:20:06 -0500

    CREACION CARPPETAS VISTA

--- pagina_web/pkt_base_de_datos/pkt_citas/Tabla_Citas.java
+++ pagina_web/pkt_base_de_datos/pkt_citas/Tabla_Citas.java
@@ -5,7 +5,14 @@ import java.sql.ResultSet;
 import pkt_conexion.enlace;
 import pkt_CRUD.Consultar;
 
-public class Tabla_Citas {
+public class Tabla_Citas extends Consultar{
+
+	@Override
+	public String BuscarRegistro_ID(String id) {
+		enlace conexion = enlace.getInstancia();
+		
+		return null;
+	}
 
 	
 
